64bd505d3ef95060c379bc46c5ebeab2373604ca
Brandon Coleman <metrix1978@gmail.com>
2021-11-16 08:49:48 -0600

    add a display statement for fun and profit.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.*;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("I got here");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
